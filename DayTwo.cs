180e4bad8e8520abd32b0a685389061e5bb79c07
ChrisNewland-bjss <chris.newland@bjss.com>
2019-12-02 17:00:11 +0000

    Started Day 2

--- DayTwo.cs
+++ DayTwo.cs
@@ -2,6 +2,13 @@ namespace AdventOfCode
 {
     public class DayTwo
     {
-        
+        public override string Compute(string[] input) {
+
+            float output = 0;
+            
+            
+            
+            return output.ToString();
+        }
     }
 }
